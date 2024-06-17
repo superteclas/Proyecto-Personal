1302a55d32a2b328c429d0334794f8a569d70e05
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-06-17 11:10:21 +0200

    iniciado nabar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/alvaroponce.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="Logo" style={{ maxHeight: "40px" }} /> 
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/bio" className="nav-link">
+                Bio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/servicios" className="nav-link">
+                Servicios
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/fotos" className="nav-link">
+                Fotos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/agenda" className="nav-link">
+                Agenda
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contacto" className="nav-link">
+                Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo" className="btn btn-primary">
+            Proyecto Empezado
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
+
+
