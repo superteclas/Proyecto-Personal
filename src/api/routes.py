450aa7b783366afe1a72a44632d053cee6e3d92f
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-06-24 21:14:25 +0000

    cambiado metodo POST en backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,30 +28,24 @@ def get_users():
     serialized_users = [user.serialize() for user in users]
     return jsonify(serialized_users), 200
 
-@api.route('/users', methods=['GET', 'POST'])
-def manage_users():
-    if request.method == 'GET':
-        # Obtener todos los usuarios
-        users = User.query.all()
-        serialized_users = [user.serialize() for user in users]
-        return jsonify(serialized_users), 200
-    elif request.method == 'POST':
-        # Agregar un nuevo usuario desde los datos proporcionados en el request
-        email = request.json.get('email')  # Asumiendo que el email se envía en formato JSON
+@api.route('/users', methods=['POST'])
+def create_user():
+    # Agregar un nuevo usuario desde los datos proporcionados en el request
+    email = request.json.get('email')  # Asumiendo que el email se envía en formato JSON
 
-        if not email:
-            return jsonify({'error': 'Email es requerido'}), 400
-        
-        # Verificar si el usuario ya existe
-        existing_user = User.query.filter_by(email=email).first()
-        if existing_user:
-            return jsonify({'error': 'El usuario ya existe'}), 400
-        
-        # Crear un nuevo usuario
-        new_user = User(email=email)
-        
-        # Guardar el nuevo usuario en la base de datos
-        db.session.add(new_user)
-        db.session.commit()
-        
-        return jsonify({'message': 'Usuario creado exitosamente'}), 201
+    if not email:
+        return jsonify({'error': 'Email es requerido'}), 400
+    
+    # Verificar si el usuario ya existe
+    existing_user = User.query.filter_by(email=email).first()
+    if (existing_user):
+        return jsonify({'error': 'El usuario ya existe'}), 400
+    
+    # Crear un nuevo usuario
+    new_user = User(email=email)
+    
+    # Guardar el nuevo usuario en la base de datos
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({'message': 'Usuario creado exitosamente'}), 201
