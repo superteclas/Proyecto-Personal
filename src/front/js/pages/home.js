47516a493a80e1303067fab9bbb8a6f7ba0b598a
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-06-28 11:08:17 +0000

    empezando a hacer el efecto de parallax en vista home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,27 +3,30 @@ import { Context } from "../store/appContext";
 import imagenhome from "../../img/piano1.png";
 import "../../styles/home.css";
 import LogoCarousel from "../component/carrusel.jsx"; 
-import SubscriptionForm from "../component/suscribe.jsx";  // Cambiar a mayúscula
+import SubscriptionForm from "../component/suscribe.jsx"; 
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<p className="imagen-home">
-				<img src={imagenhome} alt="home" />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<>
+			<div 
+				className="fixed-background"
+				style={{ backgroundImage: `url(${imagenhome})` }}
+			></div>
+			<div className="content text-center mt-5">
+				<div className="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+				
+				<div>
+					<h3>Empresas para las que he trabajado:{" "}</h3>
+					<LogoCarousel /> 
+				</div>
+				<div>
+					<SubscriptionForm/> 
+				</div>
 			</div>
-			
-			<div>
-				<h3>Empresas para las que he trabajado:{" "}</h3>
-				<LogoCarousel /> 
-			</div>
-			<div>
-				<SubscriptionForm/> 
-			</div>
-		</div>
+		</>
 	);
 };
