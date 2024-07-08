9f1df0a9ee5e848d559d38d313a4b11a690a9898
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-07-08 15:42:21 +0000

    parallax funciona

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ParallaxProvider } from "react-scroll-parallax";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-
 import { Home } from "./pages/home";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Bio } from "./pages/bio";
@@ -13,16 +12,13 @@ import { Services } from "./pages/services";
 import { Agenda } from "./pages/agenda";
 import { Contact } from "./pages/contact";
 
-//create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL === "") return <BackendURL />;
 
     return (
-        <div>
+        <ParallaxProvider>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
@@ -32,13 +28,12 @@ const Layout = () => {
                         <Route element={<Services />} path="/services" />
                         <Route element={<Agenda />} path="/agenda" />
                         <Route element={<Contact />} path="/contact" />
-                        
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
             </BrowserRouter>
-        </div>
+        </ParallaxProvider>
     );
 };
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,32 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { Context } from "../store/appContext";
+import { useParallax } from "react-scroll-parallax";
 import imagenhome from "../../img/piano1.png";
 import "../../styles/home.css";
-import LogoCarousel from "../component/carrusel.jsx"; 
-import SubscriptionForm from "../component/suscribe.jsx"; 
+import LogoCarousel from "../component/carrusel.jsx";
+import SubscriptionForm from "../component/suscribe.jsx";
+import imageBg from "../../img/piano1.png";
+import imageClose from "../../img/piano1.png";
+import imageMid from "../../img/piano1.png";
+import imageFar from "../../img/piano1.png";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+    const { store } = useContext(Context);
+    const target = useRef(null);
 
-	return (
-		<>
-			<div 
-				className="fixed-background"
-				style={{ backgroundImage: `url(${imagenhome})` }}
-			></div>
-			<div className="content text-center mt-5">
-				<div className="alert alert-info">
-					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-				</div>
-				
-				<div>
-					<h3>Empresas para las que he trabajado:{" "}</h3>
-					<LogoCarousel /> 
-				</div>
-				<div>
-					<SubscriptionForm/> 
-				</div>
-			</div>
-		</>
-	);
+    const mid = useParallax({
+        speed: 50,
+        targetElement: target.current,
+    });
+    const close = useParallax({
+        speed: 100,
+        targetElement: target.current,
+    });
+
+    const midExtend = 50 * 5 * -1;
+    const closeExtend = 100 * 5 * -1;
+
+    return (
+        <>
+            <div
+                className="fixed-background"
+                style={{ backgroundImage: `url(${imagenhome})` }}
+            ></div>
+            <div className="content text-center mt-5">
+                <div className="alert alert-info">
+                    {store.message || "Loading message from the backend (make sure your python backend is running)..."}
+                </div>
+                <div>
+                    <h3>Empresas para las que he trabajado:{" "}</h3>
+                    <LogoCarousel />
+                </div>
+                <div>
+                    <SubscriptionForm />
+                </div>
+                <div ref={target} className="parallax-container">
+                    <div style={{ backgroundImage: `url(${imageBg})`, left: 0, right: 0 }} className="parallax-layer" />
+                    <div style={{ backgroundImage: `url(${imageFar})`, left: 0, right: 0 }} className="parallax-layer" />
+                    <div style={{ backgroundImage: `url(${imageMid})`, left: midExtend, right: midExtend }} className="parallax-layer" ref={mid.ref} />
+                    <div style={{ backgroundImage: `url(${imageClose})`, left: closeExtend, right: closeExtend }} className="parallax-layer" ref={close.ref} />
+                </div>
+            </div>
+        </>
+    );
 };
