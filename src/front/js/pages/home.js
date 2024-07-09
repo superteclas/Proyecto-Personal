ee25c9ee9214c3985013b396a63ee47fea6f6635
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-07-09 09:58:02 +0200

    parallax funcionando

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useRef } from "react";
 import { Context } from "../store/appContext";
-import { useParallax } from "react-scroll-parallax";
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import imagenhome from "../../img/piano1.png";
 import "../../styles/home.css";
 import LogoCarousel from "../component/carrusel.jsx";
@@ -14,42 +14,57 @@ export const Home = () => {
     const { store } = useContext(Context);
     const target = useRef(null);
 
-    const mid = useParallax({
-        speed: 50,
-        targetElement: target.current,
-    });
-    const close = useParallax({
-        speed: 100,
-        targetElement: target.current,
-    });
-
-    const midExtend = 50 * 5 * -1;
-    const closeExtend = 100 * 5 * -1;
-
     return (
-        <>
-            <div
-                className="fixed-background"
-                style={{ backgroundImage: `url(${imagenhome})` }}
-            ></div>
-            <div className="content text-center mt-5">
-                <div className="alert alert-info">
+        <ParallaxProvider>
+           
+            
+           
+
+            <div ref={target} className="parallax-container">
+                <div className="content-section">
+                    <Parallax speed={-20} className="parallax-layer">
+                        <div style={{ backgroundImage: `url(${imageBg})` }} className="parallax-layer" />
+                    </Parallax>
+                    <div className="content">
+                        <h1>aquí iria presentacion</h1>
+                    <div className="alert alert-info">
                     {store.message || "Loading message from the backend (make sure your python backend is running)..."}
+                </div>
+                    </div>
+                </div>
+
+                <div className="content-section">
+                    <Parallax speed={-10} className="parallax-layer">
+                        <div style={{ backgroundImage: `url(${imageFar})` }} className="parallax-layer" />
+                    </Parallax>
+                    <div className="content">
+                        <h1>aquí iria un texto mas detallado</h1>
+                    </div>
+                </div>
+
+                <div className="content-section">
+                    <Parallax speed={0} className="parallax-layer">
+                        <div style={{ backgroundImage: `url(${imageMid})` }} className="parallax-layer" />
+                    </Parallax>
+                    <div className="content">
+                        <h1>aqui iria suscribirse</h1>
+                    </div>
+                </div>
+
+                <div className="content-section">
+                    <Parallax speed={10} className="parallax-layer">
+                        <div style={{ backgroundImage: `url(${imageClose})` }} className="parallax-layer" />
+                    </Parallax>
+                    <div className="content">
+                        <h2>Sección 4</h2>
+                        <p>Contenido de la sección 4...</p>
+                    </div>
                 </div>
                 <div>
                     <h3>Empresas para las que he trabajado:{" "}</h3>
                     <LogoCarousel />
                 </div>
-                <div>
-                    <SubscriptionForm />
-                </div>
-                <div ref={target} className="parallax-container">
-                    <div style={{ backgroundImage: `url(${imageBg})`, left: 0, right: 0 }} className="parallax-layer" />
-                    <div style={{ backgroundImage: `url(${imageFar})`, left: 0, right: 0 }} className="parallax-layer" />
-                    <div style={{ backgroundImage: `url(${imageMid})`, left: midExtend, right: midExtend }} className="parallax-layer" ref={mid.ref} />
-                    <div style={{ backgroundImage: `url(${imageClose})`, left: closeExtend, right: closeExtend }} className="parallax-layer" ref={close.ref} />
-                </div>
             </div>
-        </>
+        </ParallaxProvider>
     );
 };
